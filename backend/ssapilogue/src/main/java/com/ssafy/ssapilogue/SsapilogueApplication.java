7ed0a9eefadff1213b3ff45cedf773f3b1acd47b
SSAFY <gkgustj@naver.com>
2022-04-15 14:56:53 +0900

    chore: 백엔드 프로젝트 생성

---
+++ backend/ssapilogue/src/main/java/com/ssafy/ssapilogue/SsapilogueApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssapilogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsapilogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsapilogueApplication.class, args);
+	}
+
+}
---
+++ backend/ssapilogue/src/test/java/com/ssafy/ssapilogue/SsapilogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssapilogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsapilogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
