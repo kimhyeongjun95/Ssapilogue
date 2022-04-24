9972ef38a2930e5e6aa3db1eb61d3f2ee52e08f3
SSAFY <gkgustj@naver.com>
2022-04-25 02:05:55 +0900

    fix: seq domain 이름 변경
    - 몽고db Id auto increasement 용 시퀀스 이름 변경

--- backend/ssapilogue/src/main/java/com/ssafy/ssapilogue/api/service/SequenceGeneratorServiceImpl.java
+++ backend/ssapilogue/src/main/java/com/ssafy/ssapilogue/api/service/SequenceGeneratorServiceImpl.java
@@ -1,6 +1,6 @@
 package com.ssafy.ssapilogue.api.service;
 
-import com.ssafy.ssapilogue.core.domain.SurveySequence;
+import com.ssafy.ssapilogue.core.domain.DatabaseSequence;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.mongodb.core.FindAndModifyOptions;
 import org.springframework.data.mongodb.core.MongoOperations;
@@ -23,9 +23,9 @@ public class SequenceGeneratorServiceImpl implements SequenceGeneratorService{
     @Override
     public Long generateSequence(String seqName) {
 
-        SurveySequence counter = mongoOperations.findAndModify(query(where("_id").is(seqName)),
+        DatabaseSequence counter = mongoOperations.findAndModify(query(where("_id").is(seqName)),
                 new Update().inc("seq", 1), FindAndModifyOptions.options().returnNew(true).upsert(true),
-                SurveySequence.class);
+                DatabaseSequence.class);
 
         return !Objects.isNull(counter) ? counter.getSeq() : 1;
     }
--- backend/ssapilogue/src/main/java/com/ssafy/ssapilogue/core/domain/DatabaseSequence.java
+++ backend/ssapilogue/src/main/java/com/ssafy/ssapilogue/core/domain/DatabaseSequence.java
@@ -2,15 +2,14 @@ package com.ssafy.ssapilogue.core.domain;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.Id;
 
-@Document(collection = "survey_sequence")
+@Document(collection = "database_sequence")
 @Getter
 @NoArgsConstructor
-public class SurveySequence {
+public class DatabaseSequence {
 
     @Id
     private String id;
