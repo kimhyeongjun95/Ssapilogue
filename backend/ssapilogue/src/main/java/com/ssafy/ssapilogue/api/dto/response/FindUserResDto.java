32809f2f606ffbe88a31b88eb752288d108e22c3
kys980130 <kys980130@naver.com>
2022-04-25 11:43:07 +0900

    feat: 프로필 이미지 변경 및 불러오기 구현

--- backend/ssapilogue/src/main/java/com/ssafy/ssapilogue/api/dto/response/FindUserResDto.java
+++ backend/ssapilogue/src/main/java/com/ssafy/ssapilogue/api/dto/response/FindUserResDto.java
@@ -19,10 +19,13 @@ public class FindUserResDto {
 
     private String greeting;
 
+    private String image;
+
     public FindUserResDto(User user) {
         email = user.getEmail();
         nickname = user.getNickname();
         github = user.getGithub();
         greeting = user.getGreeting();
+        image = user.getImage();
     }
 }
