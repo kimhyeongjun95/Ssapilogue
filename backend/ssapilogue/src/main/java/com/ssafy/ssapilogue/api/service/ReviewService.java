c2a175f10da6450beac88dfec22092edaa690905
SSAFY <gkgustj@naver.com>
2022-04-26 16:38:15 +0900

    feat: 리뷰 등록
    - 리뷰 등록 기능 완성

--- backend/ssapilogue/src/main/java/com/ssafy/ssapilogue/api/service/ReviewService.java
+++ backend/ssapilogue/src/main/java/com/ssafy/ssapilogue/api/service/ReviewService.java
@@ -2,6 +2,8 @@ package com.ssafy.ssapilogue.api.service;
 
 import com.ssafy.ssapilogue.api.dto.request.CreateReviewReqDto;
 
+import java.util.List;
+
 public interface ReviewService {
-    Long createReview(Long projectId, CreateReviewReqDto createReviewReqDto);
+    List<String> createReview(String usrEmail, List<CreateReviewReqDto> createReviewReqDtos);
 }
--- backend/ssapilogue/src/main/java/com/ssafy/ssapilogue/api/service/ReviewServiceImpl.java
+++ backend/ssapilogue/src/main/java/com/ssafy/ssapilogue/api/service/ReviewServiceImpl.java
@@ -2,31 +2,54 @@ package com.ssafy.ssapilogue.api.service;
 
 import com.ssafy.ssapilogue.api.dto.request.CreateReviewReqDto;
 import com.ssafy.ssapilogue.core.domain.Review;
+import com.ssafy.ssapilogue.core.domain.Survey;
+import com.ssafy.ssapilogue.core.domain.SurveyOption;
 import com.ssafy.ssapilogue.core.repository.ReviewRepository;
+import com.ssafy.ssapilogue.core.repository.SurveyOptionRepository;
+import com.ssafy.ssapilogue.core.repository.SurveyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 @Transactional
 public class ReviewServiceImpl implements ReviewService{
 
+    private final SurveyRepository surveyRepository;
+
+    private final SurveyOptionRepository surveyOptionRepository;
+
     private final ReviewRepository reviewRepository;
 
     @Override
-    public Long createReview(Long projectId, CreateReviewReqDto createReviewReqDto) {
-        Review review = Review.builder()
-                .build();
+    public List<String> createReview(String userEmail, List<CreateReviewReqDto> createReviewReqDtos) {
+        List<String> reviewIds = new ArrayList<>();
 
-        if (createReviewReqDto.getIndex() != null) {
-            review.saveIndex(createReviewReqDto.getIndex());
-        } else if (createReviewReqDto.getContent() != null) {
-            review.saveContent(createReviewReqDto.getContent());
+        for (CreateReviewReqDto createReviewReqDto : createReviewReqDtos) {
+            Survey survey = surveyRepository.findById(createReviewReqDto.getSurveyId())
+                    .orElseThrow(() -> new IllegalStateException("존재하지 않는 설문조사입니다."));
+
+            Review review = Review.builder()
+                    .userEmail(userEmail)
+                    .survey(survey)
+                    .build();
+
+            if (createReviewReqDto.getSurveyOptionId() != null) {
+                SurveyOption surveyOption = surveyOptionRepository.findById(createReviewReqDto.getSurveyOptionId())
+                                .orElseThrow(() -> new IllegalStateException("존재하지 않는 옵션입니다."));
+                review.saveSurveyOption(surveyOption);
+            } else if (createReviewReqDto.getContent() != null) {
+                review.saveContent(createReviewReqDto.getContent());
+            }
+
+            Review saveReview = reviewRepository.save(review);
+            reviewIds.add(saveReview.getId());
         }
 
-        Review saveReview = reviewRepository.save(review);
-
-        return saveReview.getId();
+        return reviewIds;
     }
 }
