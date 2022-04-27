9084f493fb69e6b5c8ae33143ff72eb5b5510a79
kys980130 <kys980130@naver.com>
2022-04-27 16:12:40 +0900

    chore: docker-compose, nginx 설정

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -3,9 +3,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/api/v4', {
-      target: 'https://meeting.ssafy.com/',
+      target: 'https://meeting.ssafy.com',
       onProxyReq(proxyReq, req, res) {
-        proxyReq.setHeader('Origin','http://localhost:3000')
+        proxyReq.setHeader('Origin','http://localhost:80')
       },
       changeOrigin : true
     })
