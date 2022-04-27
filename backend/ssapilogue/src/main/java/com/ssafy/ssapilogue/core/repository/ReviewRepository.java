4191565f0ee1635d2faadb3c54eaccd154729c29
SSAFY <gkgustj@naver.com>
2022-04-28 03:47:30 +0900

    feat: 리뷰 등록
    - createdDate 수정
    - 리뷰 등록 기능 완성

--- backend/ssapilogue/src/main/java/com/ssafy/ssapilogue/core/repository/ReviewRepository.java
+++ backend/ssapilogue/src/main/java/com/ssafy/ssapilogue/core/repository/ReviewRepository.java
@@ -1,7 +1,13 @@
 package com.ssafy.ssapilogue.core.repository;
 
 import com.ssafy.ssapilogue.core.domain.Review;
+import com.ssafy.ssapilogue.core.domain.Survey;
+import com.ssafy.ssapilogue.core.domain.SurveyOption;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends MongoRepository<Review, String> {
+    List<Review> findAllBySurvey(Survey survey);
+    Integer countAllBySurveyOption(SurveyOption surveyOption);
 }
