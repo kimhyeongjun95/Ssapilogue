1eedd2219e553ec2d485787791ed833b99fe4db1
KimHyeongJun <hjthelegend@naver.com>
2022-05-02 14:30:40 +0900

    feat: 로그아웃 로직 구현

--- frontend/src/utils/store/index.js
+++ frontend/src/utils/store/index.js
@@ -2,12 +2,17 @@ import API from "../../api/API"
 
 const store = {
   setToken(token) {
-	  API.defaults.headers.common['Authorization'] = `Bearer ${token}`
-	  localStorage.setItem("jwt", `Bearer ${token}`)
+    if (token === "") {
+      localStorage.setItem("jwt", "");
+      window.location.replace("/")
+      return;
+    }
+	  API.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+	  localStorage.setItem("jwt", `Bearer ${token}`);
   },
   getToken() {
-    const token = localStorage.getItem("jwt")
-	  API.defaults.headers.common['Authorization'] = `Bearer ${token}`
+    const token = localStorage.getItem("jwt");
+	  API.defaults.headers.common['Authorization'] = `Bearer ${token}`;
     if (token) {
       return true;
     }
