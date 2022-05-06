35eef1a29ce35eaee12aae68715e399dded1800e
SSAFY <gkgustj@naver.com>
2022-05-06 16:03:02 +0900

    fix: 리뷰 예외처리
    - 안쓰는 import 삭제
    - 리뷰 예외처리

--- backend/ssapilogue/src/main/java/com/ssafy/ssapilogue/api/exception/ErrorCode.java
+++ backend/ssapilogue/src/main/java/com/ssafy/ssapilogue/api/exception/ErrorCode.java
@@ -26,6 +26,7 @@ public enum ErrorCode {
 
     /* 404 NOT_FOUND : Resource를 찾을 수 없음 */
     SURVEY_NOT_FOUND(NOT_FOUND, "해당하는 정보의 설문조사를 찾을 수 없습니다."),
+    SURVEY_OPTION_NOT_FOUND(NOT_FOUND, "해당하는 정보의 설문조사 옵션을 찾을 수 없습니다."),
 
     /* 409 : CONFLICT : Resource의 현재 상태와 충돌. 보통 중복된 데이터 존재 */
     DUPLICATE_RESOURCE(CONFLICT, "데이터가 이미 존재합니다.");
