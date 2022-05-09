e11df089fb9826101266bc78b72997bc63130096
khyunchoi <khyunchoi@naver.com>
2022-05-09 22:27:12 +0900

    chore: project 댓글 로직 수정

--- backend/ssapilogue/src/main/java/com/ssafy/ssapilogue/api/dto/request/CreateProjectCommentReqDto.java
+++ backend/ssapilogue/src/main/java/com/ssafy/ssapilogue/api/dto/request/CreateProjectCommentReqDto.java
@@ -5,14 +5,14 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @NoArgsConstructor
-@ApiModel("CreateCommentReqDto")
-public class CreateCommentReqDto {
+@ApiModel("CreateProjectCommentReqDto")
+public class CreateProjectCommentReqDto {
 
-    @ApiModelProperty(value = "프로젝트 id", example = "1", required = true)
-    private Long projectId;
-
+    @NotNull
     @ApiModelProperty(value = "댓글 내용", example = "프로젝트 잘 봤습니다!", required = true)
     private String content;
 }
