c965dd60da654cb1fa0aafe526a5ec136b86b42d
KimHyeongJun <hjthelegend@naver.com>
2022-05-10 16:24:53 +0900

    feat: 회원정보 변경 기능 구현

--- frontend/src/pages/ChangeInfoPage/index.jsx
+++ frontend/src/pages/ChangeInfoPage/index.jsx
@@ -1,23 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
 import API from "../../api/API";
 import store from "../../utils/store";
 
 const ChangeInfoPage = () => {
 
   const [inputs, setInputs] = useState({
-    nickname: '',
     github: '',
-    email: '',
     greeting: '',
   });
-  const { nickname, github, email, greeting } = inputs;
-  const navigate = useNavigate();
+  const [nickName, setNickName] = useState('');
+  const [email, setEmail] = useState('');
+  const [image, setImage] = useState('');
+  const { github, greeting } = inputs;
 
   const getInfo = async () => {
     store.getToken();
     const response = await API.get("/api/user")
     setInputs(response.data.user);
+    setEmail(response.data.user.nickName);
+    setNickName(response.data.user.email);
   }
 
   const handleOnChange = (e) => {
@@ -28,13 +29,21 @@ const ChangeInfoPage = () => {
     });
   };
 
-  const changeInfo = () => {
-    API.put("/api/user", {
+  const uploadImage = async (e) => {
+    store.getToken();
+    const formData = new FormData();
+    formData.append('file', e.target.files[0]);
+    const response = await API.post('/api/user/image', formData);
+    setImage(response.data.imageUrl);
+  }
+
+  const changeInfo = async() => {
+    await API.put("/api/user", {
       email: email,
-      nickname: nickname,
+      nickname: nickName,
       github: github,
       greeting: greeting,
-      image: null,
+      image: image,
     })
   }
   
@@ -52,12 +61,11 @@ const ChangeInfoPage = () => {
   return (
     <>
       <h1>회원정보 변경 페이지!</h1>
-      닉네임 <input name="nickname" onChange={e => handleOnChange(e)} value={nickname}/>
-      이메일 <input name="email" onChange={e => handleOnChange(e)} value={email}/>
+
+      <input type="file" onChange={uploadImage} />
       GITHUB <input name="github" onChange={e => handleOnChange(e)} value={github}/>
       자기소개 <input name="greeting" onChange={e => handleOnChange(e)} value={greeting}/>
-      <button>변경하기</button>
-
+      <button onClick={changeInfo}>변경하기</button>
       <button onClick={withDraw}>회원탈퇴</button>
     </>
   )
