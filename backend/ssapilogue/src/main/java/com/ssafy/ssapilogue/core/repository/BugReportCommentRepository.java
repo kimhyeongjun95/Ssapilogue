42ad3fe2927cf008b380556642fd27f103d8a7ac
SSAFY <gkgustj@naver.com>
2022-05-10 11:19:12 +0900

    fix: desc 적용

--- backend/ssapilogue/src/main/java/com/ssafy/ssapilogue/core/repository/BugReportCommentRepository.java
+++ backend/ssapilogue/src/main/java/com/ssafy/ssapilogue/core/repository/BugReportCommentRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface BugReportCommentRepository extends JpaRepository<BugReportComment, Long> {
-    List<BugReportComment> findAllByBugReportIdOrderById(Long bugId);
+    List<BugReportComment> findAllByBugReportIdOrderByIdDesc(Long bugId);
 }
