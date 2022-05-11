f04126bd21c733def9cabecbc79423d26b7a2588
KimHyeongJun <hjthelegend@naver.com>
2022-05-11 14:28:35 +0900

    feat: 제목 검색 기능 함수화 및 select 컴포넌트화

--- frontend/src/pages/HomePage/index.jsx
+++ frontend/src/pages/HomePage/index.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import API from "../../api/API";
+import Select from "../../components/Select"
 
 const HomePage = () => {
   
   const [searchResult, setSerachResult] = useState('');
+  const [option, setOption] = React.useState('');
 
   const getProfile = () => {
     API.get("api/user")
@@ -16,21 +18,56 @@ const HomePage = () => {
       })
   }
 
-  const search = async(e) => {
-    const value = e.target.value;
-    const response = await API.get(`/api/project/search/title?keyword=${value}`)
-    setSerachResult(response.data.searchList);
+  const handleOption = (e) => {
+    setOption(e.target.value);
+    console.log(option);
   }
 
-  const enterSearch = async (e) => {
+  // 엔터 검색은 실제 프로젝트 보여주기
+  // 자동 완성은 드롭다운
+  const titleEnterSearch = async (e, value) => {
+    const res = await API.get(`/api/project//search?keyword=${value}`)
+    setSerachResult(res.data.projectList);
+    e.target.value = "";
+    console.log("제목 엔터검색")
+  }
+  const titleAutoSearch = async (value) => {
+    const response = await API.get(`/api/project/search/title?keyword=${value}`)
+    setSerachResult(response.data.searchList);
+    console.log("제목 자동검색")
+  }
+
+  const search = async(e) => {
     const value = e.target.value;
-    if (e.key === "Enter") {
-      const res = await API.get(`/api/project//search?keyword=${value}`)
-      setSerachResult(res.data.projectList);
-      e.target.value = "";
-      return;
+    try {
+      if (option === "제목") {
+        if (e.key === "Enter") {
+          titleEnterSearch(e, value);
+          return;
+        }
+        titleAutoSearch(value);
+        return;
+      }
+
+      // if (option === "기술스택") {
+        
+      // }
+    } catch (e) {
+      throw e;
     }
   }
+
+
+
+  // 기술스택 검색 자동완성
+  // 1. 기술스택 검색 => 프로젝트 제목 나옴
+  // 2. 기술스택 이름의 자동완성
+
+  // 실제 검색(엔터 or 클릭)
+  // 1. 엔터 : 기술스택 검색 => 실제 프로젝트 카드 나옴 (react, react native 중복 O)
+  // 2. 클릭 : 기술스택 클릭 => 이 기술과 관련된 것만 (react, react native 중복 X)
+
+  // 
 
   return (
     <>
@@ -45,7 +82,8 @@ const HomePage = () => {
       <button onClick={getProfile}>프로필 조회</button>
 
       <br />
-      <input type="text" onChange={e => search(e)} onKeyPress={enterSearch} />
+      <Select onChange={handleOption} option={option} />
+      <input type="text" onChange={e => search(e)} onKeyPress={(e) => search(e)} />
       <br />
       {searchResult && searchResult.map((search) => (
         <>
