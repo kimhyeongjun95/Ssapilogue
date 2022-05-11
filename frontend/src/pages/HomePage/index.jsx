2d2d429c16b1ec358867071ea1eb2dd543cc9d78
KimHyeongJun <hjthelegend@naver.com>
2022-05-11 10:52:46 +0900

    feat: 제목 검색 자동완성 및 검색 기능 구현

--- frontend/src/pages/HomePage/index.jsx
+++ frontend/src/pages/HomePage/index.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import API from "../../api/API";
 
 const HomePage = () => {
   
+  const [searchResult, setSerachResult] = useState('');
+
   const getProfile = () => {
     API.get("api/user")
       .then((res) => {
@@ -14,13 +16,43 @@ const HomePage = () => {
       })
   }
 
+  const search = async(e) => {
+    const value = e.target.value;
+    const response = await API.get(`/api/project/search/title?keyword=${value}`)
+    setSerachResult(response.data.searchList);
+  }
+
+  const enterSearch = async (e) => {
+    const value = e.target.value;
+    if (e.key === "Enter") {
+      const res = await API.get(`/api/project//search?keyword=${value}`)
+      setSerachResult(res.data.projectList);
+      e.target.value = "";
+      return;
+    }
+  }
+
   return (
     <>
+
       <h1>여기는 메인화면</h1>
+
+
       <Link to="/project/post">
         <button>지금 등록하기</button>
       </Link>
+
       <button onClick={getProfile}>프로필 조회</button>
+
+      <br />
+      <input type="text" onChange={e => search(e)} onKeyPress={enterSearch} />
+      <br />
+      {searchResult && searchResult.map((search) => (
+        <>
+          {search.title}
+        </>
+      ))}
+
     </>
   )
 }
