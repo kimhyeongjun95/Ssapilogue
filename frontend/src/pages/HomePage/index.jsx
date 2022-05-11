e10ab778ce6d5fd78e2079d154b7ce4a7b8d90bc
KimHyeongJun <hjthelegend@naver.com>
2022-05-11 16:27:46 +0900

    feat: 프로젝트 제목, 기술스택 별 검색 기능 구현

--- frontend/src/pages/HomePage/index.jsx
+++ frontend/src/pages/HomePage/index.jsx
@@ -5,42 +5,58 @@ import Select from "../../components/Select"
 
 const HomePage = () => {
   
-  const [searchResult, setSerachResult] = useState('');
+  const [searchResult, setSearchResult] = useState('');
+  const [dropResult, setDropResult] = useState('');
+  const [techSearchResult, setTechSearchResult] = useState('');
   const [option, setOption] = React.useState('');
-
-  const getProfile = () => {
-    API.get("api/user")
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-  }
 
   const handleOption = (e) => {
     setOption(e.target.value);
-    console.log(option);
   }
 
-  // 엔터 검색은 실제 프로젝트 보여주기
-  // 자동 완성은 드롭다운
   const titleEnterSearch = async (e, value) => {
-    const res = await API.get(`/api/project//search?keyword=${value}`)
-    setSerachResult(res.data.projectList);
+    const response = await API.get(`/api/project//search?keyword=${value}`);
+    setSearchResult(response.data.projectList);
+    setDropResult('');
     e.target.value = "";
-    console.log("제목 엔터검색")
   }
+
   const titleAutoSearch = async (value) => {
-    const response = await API.get(`/api/project/search/title?keyword=${value}`)
-    setSerachResult(response.data.searchList);
-    console.log("제목 자동검색")
+    const response = await API.get(`/api/project/search/title?keyword=${value}`);
+    setDropResult(response.data.searchList);
+  }
+
+  const techProjectAutoSearch = async (value) => {
+    const response = await API.get(`/api/tech-stack/search/title?keyword=${value}`);
+    setDropResult(response.data.searchList);
+  }
+
+  const techStackAutoSearch = async (value) => {
+    const response = await API.get(`/api/tech-stack/search/specific?keyword=${value}`);
+    setTechSearchResult(response.data.searchList)
+  }
+
+  const techProjectEnterSearch = async (e, value) => {
+    const response = await API.get(`/api/tech-stack/search/project?keyword=${value}`);
+    setSearchResult(response.data.projectList);
+    setDropResult('');
+    setTechSearchResult('');
+    e.target.value = "";
+  }
+
+  const techStackClickSearch = async (e) => {
+    const value = e.target.innerText;
+    const response = await API.get(`/api/tech-stack//search/specific/project?keyword=${value}`);
+    setSearchResult(response.data.projectList);
+    setDropResult('');
+    setTechSearchResult('');
   }
 
   const search = async(e) => {
     const value = e.target.value;
     try {
       if (option === "제목") {
+        setTechSearchResult('');
         if (e.key === "Enter") {
           titleEnterSearch(e, value);
           return;
@@ -49,25 +65,19 @@ const HomePage = () => {
         return;
       }
 
-      // if (option === "기술스택") {
-        
-      // }
+      if (option === "기술스택") {
+        if (e.key === "Enter") {
+          techProjectEnterSearch(e, value);
+          return;
+        }
+        techProjectAutoSearch(value);
+        techStackAutoSearch(value);
+        return;
+      }
     } catch (e) {
       throw e;
     }
   }
-
-
-
-  // 기술스택 검색 자동완성
-  // 1. 기술스택 검색 => 프로젝트 제목 나옴
-  // 2. 기술스택 이름의 자동완성
-
-  // 실제 검색(엔터 or 클릭)
-  // 1. 엔터 : 기술스택 검색 => 실제 프로젝트 카드 나옴 (react, react native 중복 O)
-  // 2. 클릭 : 기술스택 클릭 => 이 기술과 관련된 것만 (react, react native 중복 X)
-
-  // 
 
   return (
     <>
@@ -79,18 +89,31 @@ const HomePage = () => {
         <button>지금 등록하기</button>
       </Link>
 
-      <button onClick={getProfile}>프로필 조회</button>
-
       <br />
       <Select onChange={handleOption} option={option} />
       <input type="text" onChange={e => search(e)} onKeyPress={(e) => search(e)} />
       <br />
-      {searchResult && searchResult.map((search) => (
-        <>
+      <h3>프로젝트 미리보기 검색 결과</h3>
+      {dropResult && dropResult.map((search, idx) => (
+        <div key={idx}>
           {search.title}
-        </>
+        </div>
+      ))}
+      <h3>기술 검색 결과</h3>
+      {techSearchResult && techSearchResult.map((tech, idx) => (
+        <div key={idx}>
+          <h6 onClick={techStackClickSearch}>
+            {tech}
+          </h6>
+        </div>
       ))}
 
+      <h3>프로젝트 최종 검색 결과</h3>
+      {searchResult && searchResult.map((search, idx) => (
+        <div key={idx}>
+          {search.title}
+        </div>
+      ))}
     </>
   )
 }
