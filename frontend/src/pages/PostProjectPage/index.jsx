78f3c23090ae4bbe1f504888dd2ec90f174f5f1d
Tannent <strawberry01128@gmail.com>
2022-05-13 16:03:53 +0900

    feat: 입력별분기처리완료

--- frontend/src/pages/PostProjectPage/index.jsx
+++ frontend/src/pages/PostProjectPage/index.jsx
@@ -8,11 +8,13 @@ import API from '../../api/API';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { Editor } from '@toast-ui/react-editor';
 import { Link, useNavigate } from 'react-router-dom';
-import "./style.scss"
+import Alert from '@mui/material/Alert';
+import swal from 'sweetalert';
+import { compileString } from 'sass';
 
 const PostProjectPage = () => {
-  let navigate = useNavigate()
 
+  const navigate = useNavigate()
   // 상태관리
   const [title, setTitle] = useState('')
   const [bepo, setBepo] = useState('') 
@@ -121,20 +123,21 @@ const PostProjectPage = () => {
  
   //
    
-  const hashType = (InputTitle,Placeholder,inputBox,inputValue,inputSetValue,inputSetbox,hamsu) => {
+  const hashType = (InputTitle,Plcaehorder, inputBox,inputValue,inputSetValue,inputSetbox,hamsu,inputId) => {
     const handleChange = (event) => {
       inputSetValue(event.target.value);
     };
     return <div style={{width: "40%"}}>
       <p style={{marginBottom : 0}}> {InputTitle} </p>
-      <input
+      <TextField
         type="text"
         size = "small"
-        className='post-question-style'
+        className={inputId}
+        style={{width:"100%"}}
         value={inputValue}
         onChange={handleChange}
         onKeyPress={hamsu}
-        placeholder={Placeholder}
+        placeholder={Plcaehorder}
       />
       <div style={{width:"100%" ,display:"flex",flexDirection:"row", alignItems :"center",flexWrap: "wrap"}}>
         {alHash(inputBox,inputSetbox)}
@@ -148,7 +151,7 @@ const PostProjectPage = () => {
     const handleChange = (event) => {
       setVarious(event.target.value);
     };
-    return  <FormControl style={{marginTop:"2vh",width:"40%",marginLeft: "auto", marginRight:"auto"}} required size="small">
+    return  <FormControl style={{marginTop:"2vh",width:"40%"}} size="small">
       <InputLabel id="demo-select-small">* 분류</InputLabel>
       <Select
         labelId="demo-select-small"
@@ -170,68 +173,91 @@ const PostProjectPage = () => {
     console.log(typeof e.target.value)
   }
 
-
-  function gotoSurvey (event) {
-    event.preventDefault();
-    console.log(event)
-
-    navigate('/project/survey', {
-      state : {
-        title: title,
-        intro: intro,
-        various: various,
-        phashbox: phashbox,
-        hashbox: hashbox,
-        bepo: bepo,
-        repo: repo,
-        thumbnail: thumbnail,
-        readmeCheck: readmeCheck,
-        markdown: markdown
+  const toSurvey = () => {
+    let party = [title, various, repo, hashbox,phashbox,intro]
+    let flag = false
+    let party_name = ["프로젝트이름","분류", "Git repo", "기술스택", "프로젝트 멤버","프로젝트 소개"]
+    for (let party_index = 0; party_index < party.length; party_index++ ) {
+      if (party[party_index].length === 0)
+      {
+        if ( party_index === 1) {
+          flag = true
+          swal("미입력", `${party_name[party_index]}이(가) 입력되지 않았습니다.`, "error");
+          break
+        }else {
+          console.log(party_name[party_index])
+          let a = document.getElementsByClassName(`${party_name[party_index]}`)
+          console.log(a[0].querySelector('input'))
+          document.getElementsByClassName(`${party_name[party_index]}`)[0].querySelector('input').focus()
+          flag = true
+          swal("미입력", `${party_name[party_index]}이(가) 입력되지 않았습니다.`, "error");
+          break
+        }
       }
-    })
+  
+    }
+    if (!flag) {
+      navigate('/project/survey',{
+        state : {
+          title: title,
+          intro: intro,
+          various: various,
+          phashbox: phashbox,
+          hashbox: hashbox,
+          bepo: bepo,
+          repo: repo,
+          thumbnail: thumbnail,
+          readmeCheck: readmeCheck,
+          markdown: markdown
+        }
+      })
+    }
+    
+   
   }
+
+ 
+
   return (
     <>
       <div style={{display:"flex",flexDirection:"column", justifyContent : "center", alignItems :"center"}}>
 
         <h2 style={{width:"40%", textAlign:"center"}}>프로젝트를 등록해주세요</h2>
         <p style={{width:"40%", color: "#909090", fontSize: 12,marginBottom:"5vh"}}> * 는 필수항목입니다.</p>
-        <form onSubmit={gotoSurvey} id="postProject" style={{width:"100%", display:"flex",flexDirection:"column", justifyContent : "center", alignItems :"center"}}>
-          <Question InputTitle="* 프로젝트 이름" inputValue={title} inputSetValue={setTitle} pilsu="1"/>
-          {chooseType()}
-          <Question InputTitle="배포주소" inputValue={bepo} inputSetValue={setBepo} />
-          <Question InputTitle="* Git Repo" inputValue={repo} inputSetValue={setRepo} pilsu="1"/>
-        
-          {hashType("* 기술스택","* 기술스택을 누르고 엔터를 누르세요!",hashbox,hashtag,setHashtag,setHashbox,plusHashtag)}
-          {hashType("* 프로젝트 멤버" ,"* 프로젝트 멤버를 입력하고 엔터를 누르세요!", phashbox,phashtag,setpHashtag,setpHashbox,PplusHashtag)}
+        <Question InputTitle="* 프로젝트 이름" inputValue={title} inputSetValue={setTitle} pilsu="1" inputId="프로젝트이름"/>
+        {chooseType()}
+        <Question InputTitle="배포주소" inputValue={bepo} inputSetValue={setBepo} />
+        <Question InputTitle="* Git Repo" inputValue={repo} inputSetValue={setRepo} pilsu="1" inputId="Git repo"/>
+        {hashType("* 기술스택","기술스택을 입력해주세요",hashbox,hashtag,setHashtag,setHashbox,plusHashtag,"기술스택")}
+        {hashType("* 프로젝트 멤버","프로젝트 멤버를 입력해주세요",phashbox,phashtag,setpHashtag,setpHashbox,PplusHashtag,"프로젝트 멤버")}
 
-          <input type="file" style={{ display: "none" }} onChange={onChange} ref={imageInput} accept="img/*" />
-          <button style={uploadButton} onClick={onCickImageUpload}>썸네일 업로드</button>
-          { (thumbnail) ?
-            <div style={{marginTop:"2vh"}}>
-              <img src={thumbnailUrl} style={{height:"20vh",width:"35vh"}} alt="thumbnail" />
-            </div>
-            :
-            null
-          }
-          <div>
-            <input type="radio" checked={readmeCheck === "1"} name="theme" value={"1"} onChange={mkChange} />직접 입력하기
-            <input type="radio" checked={readmeCheck === "0"} name="theme" value={"0"} onChange={mkChange}/>github에서 가져오기
+        <input type="file" style={{ display: "none" }} onChange={onChange} ref={imageInput} accept="img/*" />
+        <button style={uploadButton} onClick={onCickImageUpload}>썸네일 업로드</button>
+        { (thumbnail) ?
+          <div style={{marginTop:"2vh"}}>
+            <img src={thumbnailUrl} style={{height:"20vh",width:"35vh"}} alt="thumbnail" />
           </div>
-          <Question InputTitle="* 소개" inputValue={intro} inputSetValue={setIntro} pilsu="1"/>
-          { (readmeCheck === "1") ?
-            <div style={{marginTop:"2%",width:"40%"}}>
-              <Editor
-                initialEditType="markdown"
-                height="40vh"
-                placeholder='마크다운을 붙여주세요.'
-                onChange={onChangeIntroFunction}
-                ref={editorRef}
-              /> 
-            </div>
-            : null
-          }
-        </form>
+          :
+          null
+        }
+        <div>
+          <input type="radio" checked={readmeCheck === "1"} name="theme" value={"1"} onChange={mkChange} />직접 입력하기
+          <input type="radio" checked={readmeCheck === "0"} name="theme" value={"0"} onChange={mkChange}/>github에서 가져오기
+        </div>
+        <Question InputTitle="* 소개" inputValue={intro} inputSetValue={setIntro} pilsu="1" inputId="프로젝트 소개"/>
+        { (readmeCheck === "1") ?
+          <div style={{marginTop:"2%",width:"40%"}}>
+            <Editor
+              initialEditType="markdown"
+              height="40vh"
+              placeholder='마크다운을 붙여주세요.'
+              onChange={onChangeIntroFunction}
+              ref={editorRef}
+            /> 
+          </div>
+          : null
+        }
+
         <div style={{display:"flex",flexDirection:"row", marginTop:"5vh",marginBottom:"5vh"}}>
           <Button size="large" style={{marginRight:"3vw"}} variant="outlined"> 취소 </Button>
           {/* <Link 
@@ -249,7 +275,7 @@ const PostProjectPage = () => {
               markdown: markdown
             }}
           > */}
-          <Button type="submit" form="postProject" size="large" variant="contained"> 다음단계 </Button>
+          <Button onClick={toSurvey} size="large" variant="contained"> 다음단계 </Button>
           {/* </Link> */}
         </div>
 
