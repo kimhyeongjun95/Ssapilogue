e4ec6aa04a5edd83751410c9e538971eb450aa03
KimHyeongJun <hjthelegend@naver.com>
2022-05-18 11:24:56 +0900

    i# HEADER: [지라번호] <Type>: <Title> fix: 검색 필터링

--- frontend/src/components/Card/index.jsx
+++ frontend/src/components/Card/index.jsx
@@ -6,8 +6,6 @@ import Comment from "../../assets/comment.png"
 import BookMarked from '../../assets/BookMarked.png'
 import NotBookMarked from '../../assets/NotBookMarked.png'
 import Default from '../../assets/SSAFY.png'
-import API from "../../api/API"
-import store from '../../utils/store'
 
 const Card = ({ title, content, category, likeCnt, viewCnt, commentCnt, techStack, thumbnail, bookmark, projectId }) => {
 
